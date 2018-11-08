dd60ad8b5fc2a12d71b62ade11fd7986ac3e6ecc
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2018-11-09 04:12:03 +0530

    scroling effects
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- js/main.js
+++ js/main.js
@@ -8,12 +8,17 @@ $(document).ready(function() {
     $('body,html').animate({
       scrollTop: $(this.hash).offset().top
     }, 1000 );
+
   });
 
   // Active link switching
   $(window).scroll(function() {
     var scrollbarLocation = $(this).scrollTop();
-
+    // $('div').animate({left:'250px'});
+    // console.log(scrollbarLocation)
+    // if(scrollbarLocation === 522){
+    //   $('.skill-wrapper').animate({left:'250px'})
+    // }
     scrollLink.each(function() {
 
       var sectionOffset = $(this.hash).offset().top - 20;
@@ -21,10 +26,15 @@ $(document).ready(function() {
       if ( sectionOffset <= scrollbarLocation ) {
         $(this).parent().addClass('active');
         $(this).parent().siblings().removeClass('active');
-      }
-    })
 
-  })
+
+      }
+        });
+
+      });
+
+
+
 
 
 
@@ -37,28 +47,30 @@ $(document).ready(function() {
 
       });
 
-    $('.hello .letters').each(function(){
-      $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+
+  $('.hello .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+
+  anime.timeline({loop: true})
+    .add({
+      targets: '.hello .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 500,
+      delay: function(el, i) {
+        return 50 * i;
+      }
+    }).add({
+      targets: '.hello',
+      opacity: 100,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 500
     });
 
-    anime.timeline({loop: true})
-      .add({
-        targets: '.hello .letter',
-        translateY: ["1.1em", 0],
-        translateZ: 0,
-        duration: 500,
-        delay: function(el, i) {
-          return 50 * i;
-        }
-      }).add({
-        targets: '.hello',
-        opacity: 100,
-        duration: 1000,
-        easing: "easeOutExpo",
-        delay: 500
-      });
 
-      
+
 
 
 });
